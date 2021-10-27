1b9735fe38ba9d652a44d474e54e9b8320b01232
O-Jun-S <Jun.Shima256@gmail.com>
2021-10-27 16:11:32 +0000

    Fix an syntax issue.

--- OjunLoaderPkg/Main.c
+++ OjunLoaderPkg/Main.c
@@ -28,7 +28,7 @@ EFI_STATUS GetMemoryMap(struct MemoryMap *map) {
   map->map_size = map->buffer_size;
   return gBS->GetMemoryMap(
     &map->map_size,
-    (*EFI_MEMORY_DESCRIPTOR)&map,
+    (EFI_MEMORY_DESCRIPTOR*)&map,
     &map->map_key,
     &map->descriptor_size,
     &map->descriptor_version,
