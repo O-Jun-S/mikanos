8320e64d25de593bc6d279d80530dee05860da31
O-Jun-S <Jun.Shima256@gmail.com>
2021-10-27 19:13:30 +0000

    Write code to draw white.

--- OjunLoaderPkg/Main.c
+++ OjunLoaderPkg/Main.c
@@ -202,6 +202,30 @@ EFI_STATUS EFIAPI UefiMain(
   entry_point();
   // #@@range_end(boot_kernel)
 
+  // #@@range_start(draw_white)
+  EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
+  OpenGOP(image_handle, &gop);
+  Print(
+    L"Resolution: %ux%u, Pixel Format: %s, %u, pixels/line\n",
+    gop->Mode->Info->HorizontalResolution,
+    gop->Mode->Info->VerticalResolution,
+    GetPixelFormatUnicode(gop->Mode->Info->PixelFormat),
+    gop->Mode->Info->PixelsPerScanLine
+  );
+
+  Print(
+    L"Frame Buffer: 0x%0lx - 0x%0lx, Size: %lu bytes\n",
+    gop->Mode->FrameBufferBase,
+    gop->Mode->FrameBufferBase + gop->Mode->FrameBufferSize,
+    gop->Mode->FrameBufferSize
+  );
+
+  UINT8 *frame_buffer = (UINT8*)gop->Mode->FrameBufferBase;
+  for(UINTN i = 0; i < gop->Mode->FrameBufferSize; i++) {
+    frame_buffer[i] = 255;
+  }
+  // #@@range_end(draw_white)
+
   Print(L"I done!!!\n");
 
   while (1);
