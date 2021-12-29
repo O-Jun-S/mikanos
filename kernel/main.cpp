211282ca70ca7e9661b096c23181bad876172077
O-Jun-S <Jun.Shima256@gmail.com>
2021-12-29 02:21:10 +0000

    Delete 'WritePixel' function.

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -55,37 +55,6 @@ class BGRResv8BitPerColorPixelWriter: public PixelWriter {
         }
 };
 
-
-/** WritePixel draw one point.
- * @retval 0        success
- * @retval not 0    failed
- */
-int WritePixel(
-    const FrameBufferConfig& config,
-    int x, int y, const PixelColor& c
-) {
-    const int pixel_position = config.pixels_per_scan_line * y + x;
-    if(config.pixel_format == kPixelRGBResv8BitPerColor) {
-        uint8_t* p = &config.frame_buffer[4 * pixel_position];
-        p[0] = c.r;
-        p[1] = c.g;
-        p[2] = c.b;
-    }
-
-    else if (config.pixel_format == kPixelBGRResv8BitPerColor) {
-        uint8_t* p = &config.frame_buffer[4 * pixel_position];
-        p[0] = c.b;
-        p[1] = c.g;
-        p[2] = c.r;
-    }
-
-    else {
-        return -1;
-    }
-
-    return 0;
-}
-
 extern "C" void KernelMain(
     FrameBufferConfig& frame_buffer_config
 ) {
