e6c429387c22177c52b040600834ff0916907167
O-Jun-S <Jun.Shima256@gmail.com>
2021-12-29 02:20:40 +0000

    Add classes to write something.

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -6,6 +6,56 @@ struct PixelColor {
 };
 
 
+// PixelWriterインターフェイス
+class PixelWriter {
+    public:
+        // コンストラクタ
+        // Write関数を呼び出すたびにconfigを受け取る必要がなくなる。
+        PixelWriter(const FrameBufferConfig &config) : config_{config} {
+        }
+
+        // デストラクタ
+        virtual ~PixelWriter() = default;
+
+        // = 0 → 純粋仮想関数。実装を持たないインターフェイス。
+        virtual void Write(int x, int y, const PixelColor &c) = 0;
+
+    protected:
+        uint8_t* PixelAt(int x, int y) {
+            return config_.frame_buffer + 4 * (config_.pixels_per_scan_line * y + x);
+        }
+    
+    private:
+        const FrameBufferConfig &config_;
+};
+
+
+class RGBResv8BitPerColorPixelWriter : public PixelWriter {
+    public:
+        using PixelWriter::PixelWriter;
+    
+        virtual void Write(int x, int y, const PixelColor &c) override {
+            auto p = PixelAt(x, y);
+            p[0] = c.r;
+            p[1] = c.g;
+            p[2] = c.b;
+        }
+};
+
+
+class BGRResv8BitPerColorPixelWriter: public PixelWriter {
+    public:
+        using PixelWriter::PixelWriter;
+
+        virtual void Write(int x, int y, const PixelColor &c) override {
+            auto p = PixelAt(x, y);
+            p[0] = c.b;
+            p[1] = c.g;
+            p[2] = c.r;
+        }
+};
+
+
 /** WritePixel draw one point.
  * @retval 0        success
  * @retval not 0    failed
