e3325a4513cd424027d1fcee75be0420c4151204
O-Jun-S <Jun.Shima256@gmail.com>
2021-12-29 11:25:51 +0000

    フォントに関する部分をfont.cppと.hppに分割

---
+++ kernel/font.hpp
@@ -0,0 +1,25 @@
+#include"graphics.hpp"
+#include<cstdint>
+
+// #@@range_begin(font_a)
+const uint8_t kFontA[16] = {
+  0b00000000, //
+  0b00011000, //    **
+  0b00011000, //    **
+  0b00011000, //    **
+  0b00011000, //    **
+  0b00100100, //   *  *
+  0b00100100, //   *  *
+  0b00100100, //   *  *
+  0b00100100, //   *  *
+  0b01111110, //  ******
+  0b01000010, //  *    *
+  0b01000010, //  *    *
+  0b01000010, //  *    *
+  0b11100111, // ***  ***
+  0b00000000, //
+  0b00000000, //
+};
+// #@@range_end(font_a)
+
+void WriteAscii(PixelWriter &writer, int x, int y, char c, const PixelColor &color);
