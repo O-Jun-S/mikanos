fa44ff0b2e025f1c8afa526b7cb6fc9b220de05f
O-Jun-S <Jun.Shima256@gmail.com>
2021-12-29 02:47:15 +0000

    定義したクラスを使って緑の長方形を描いた。

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,4 +1,5 @@
 #include<cstdint>
+#include <cstddef>
 #include"frame_buffer_config.hpp"
 
 struct PixelColor {
@@ -55,18 +56,44 @@ class BGRResv8BitPerColorPixelWriter: public PixelWriter {
         }
 };
 
+
+void* operator new(size_t size, void *buf) {
+    return buf;
+}
+
+
+void operator delete(void *obj) noexcept {
+}
+
+
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+PixelWriter *pixel_writer;
+
+
 extern "C" void KernelMain(
     FrameBufferConfig& frame_buffer_config
 ) {
+    switch(frame_buffer_config.pixel_format) {
+        case kPixelRGBResv8BitPerColor:
+            pixel_writer = new(pixel_writer_buf)
+                RGBResv8BitPerColorPixelWriter{frame_buffer_config};
+            break;
+
+        case kPixelBGRResv8BitPerColor:
+            pixel_writer = new(pixel_writer_buf)
+                BGRResv8BitPerColorPixelWriter{frame_buffer_config};
+            break;
+    }
+
     for(int x=0; x<frame_buffer_config.horizontal_resolution; x++) {
         for(int y=0; y<frame_buffer_config.vertical_resolution; y++) {
-            WritePixel(frame_buffer_config, x, y, {255, 255, 255});
+            pixel_writer->Write(x, y, {255, 255, 255});
         }
+    }
 
-        for(int x=0; x<200; x++) {
-            for(int y=0; y<100; y++) {
-                WritePixel(frame_buffer_config, 100+x, 100+y, {0, 100, 100});
-            }
+    for(int x=0; x<200; x++) {
+        for(int y=0; y<100; y++) {
+            pixel_writer->Write(x, y, {0, 255, 0});
         }
     }
     while(1) __asm__("hlt");
